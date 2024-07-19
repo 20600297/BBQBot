b99446678c174f3899f70e353ff1b3e458f60dad
20600297 <20600297@qq.com>
2024-07-19 11:44:30 +0800

    1.0.3 添加后台管理页面

---
+++ src/main/java/indi/wzq/BBQBot/controller/LoginController.java
@@ -0,0 +1,12 @@
+package indi.wzq.BBQBot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/login")
+    public String login(){return "login";}
+
+}
